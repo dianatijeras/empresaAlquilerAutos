e8fdc9acae6c777944109352b9e5a40eb3d83bb2
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2024-11-05 15:12:40 -0500

    Initial commit

---
+++ src/main/java/co/edu/uniquindio/poo/empresaalquilerautos/controller/InicioController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.poo.empresaalquilerautos.controller;
+
+public class InicioController {
+}
---
+++ src/main/java/co/edu/uniquindio/poo/empresaalquilerautos/viewController/VehiculoViewController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.poo.empresaalquilerautos.viewController;
+
+public class VehiculoViewController {
+}
